9a524d4ddbd93525b67c3abd5e075c6f93879311
Bang Prass <langgengprassadewo@gmail.com>
2026-05-03 16:57:03 +0800

    Initial Commit

---
+++ config/config.php
@@ -0,0 +1,3 @@
+<?php
+// define base url
+define('BASE_URL', 'http://NomadStock.test/');
---
+++ public/index.php
@@ -0,0 +1,26 @@
+<?php
+
+// 1. load configuration
+require_once('../config/config.php');
+echo "Base URL: " . BASE_URL;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Nomad Biola Stock </title>
+
+    <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/style.css">
+</head>
+
+<body>
+    <!-- Header Section -->
+
+
+</body>
+
+</html>
